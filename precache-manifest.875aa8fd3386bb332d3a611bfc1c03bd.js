f8b5f33581a18bb13d5af8528345fa5ce8152dc7
Andrei <mobilelookfree@gmail.com>
2018-11-26 21:16:41 +0300

    Updates

---
+++ precache-manifest.875aa8fd3386bb332d3a611bfc1c03bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13dd45940cdb2a5461cb",
+    "url": "/itschool/static/js/runtime~main.13dd4594.js"
+  },
+  {
+    "revision": "96da4fe0f58ffb7566cd",
+    "url": "/itschool/static/js/main.96da4fe0.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/itschool/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "96da4fe0f58ffb7566cd",
+    "url": "/itschool/static/css/main.4fd4a6eb.chunk.css"
+  },
+  {
+    "revision": "4420b0c337952919a2bebd34cc21c1b4",
+    "url": "/itschool/index.html"
+  }
+];
