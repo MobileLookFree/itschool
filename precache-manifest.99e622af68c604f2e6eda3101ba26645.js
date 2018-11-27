89e063cd1e10a22b0787f41a20fb15acaf97ca8a
Andrei <mobilelookfree@gmail.com>
2018-11-27 21:30:24 +0300

    Updates

---
+++ precache-manifest.99e622af68c604f2e6eda3101ba26645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13dd45940cdb2a5461cb",
+    "url": "/itschool/static/js/runtime~main.13dd4594.js"
+  },
+  {
+    "revision": "d39abc1c4b973aaea428",
+    "url": "/itschool/static/js/main.d39abc1c.chunk.js"
+  },
+  {
+    "revision": "b0bd6a8c8897731ea6c6",
+    "url": "/itschool/static/js/1.b0bd6a8c.chunk.js"
+  },
+  {
+    "revision": "d39abc1c4b973aaea428",
+    "url": "/itschool/static/css/main.39077897.chunk.css"
+  },
+  {
+    "revision": "518d91e991b516e5a9db09cfe3b034dc",
+    "url": "/itschool/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/itschool/precache-manifest.875aa8fd3386bb332d3a611bfc1c03bd.js"
+  "/itschool/precache-manifest.99e622af68c604f2e6eda3101ba26645.js"
 );
 
 workbox.clientsClaim();
