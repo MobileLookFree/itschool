d95be3f2fe5fd9ae153b261716b125710bc70d4e
Andrei <mobilelookfree@gmail.com>
2018-12-06 00:42:35 +0300

    gh pages basename fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ class App extends Component {
 
     render() {
         return (
-            <Router history={history}>
+            <Router history={history} basename={process.env.PUBLIC_URL}>
                 <div>
                     <Switch>
                         <Route exact path="/" component={MainPage} />
